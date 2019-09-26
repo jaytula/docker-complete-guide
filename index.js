b6c93e1dc323df042d586c7ebf0cfb25f084a4e5
Jay T <jaytula@gmail.com>
2019-09-26 11:09:11 -0700

    App Server Code

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const redis = require("redis");
+
+const PORT = 8081;
 
 const app = express();
+const client = redis.createClient();
+
+client.set('visits', 0);
 
 app.get("/", (req, res) => {
-  res.send("How are you doing");
+  client.get("visits", (err, visits) => {
+    res.send("Number of visits is " + visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
 });
 
-const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`simpleweb running on ${PORT}`);
 });
